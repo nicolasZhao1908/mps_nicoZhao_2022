bddf5544cfeffcd293c7a525f771e4dd82def00f
nicolasZhao1908 <0619971117@uma.es>
2022-02-25 11:51:07 +0100

    Refactor Factorial class

--- src/main/java/org/zzlnico/factorial/Factorial.java
+++ src/main/java/org/zzlnico/factorial/Factorial.java
@@ -3,18 +3,12 @@ package org.zzlnico.factorial;
 public class Factorial {
 
     public int compute(int value) {
-        int result;
-        if (value < 0){
+        if (value < 0) {
             throw new RuntimeException("The value is negative: " + value);
         }
-        if((value == 0) || (value == 1)){
-            result = 1;
-        }else if (value == 2){
-            result = 2;
-        }else if (value == 3){
-            result = 6;
-        }else{
-            result = value * compute(value - 1);
+        int result = 1;
+        for (int i = 1; i <= value; i++) {
+            result *= i;
         }
         return result;
     }
